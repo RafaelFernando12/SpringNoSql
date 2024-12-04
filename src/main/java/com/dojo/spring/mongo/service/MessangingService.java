6bb3f5f47e6e6d2f2f4137c302a1ebb7082dddd2
RafaelFernando12 <rafael.bergmann@hotmail.com>
2024-12-04 12:54:54 -0300

    fix: corrige envio de mensagem serializando o objeto

--- src/main/java/com/dojo/spring/mongo/service/MessangingService.java
+++ src/main/java/com/dojo/spring/mongo/service/MessangingService.java
@@ -2,16 +2,23 @@ package com.dojo.spring.mongo.service;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessangingService {
 
     @Autowired
-    private  RabbitTemplate rabbitTemplate;
+    private RabbitTemplate rabbitTemplate;
 
-    public void sendMessage(String queueName, Object message) {
-        rabbitTemplate.convertAndSend(message);
+    @Value("${rabbitmq.routing.key.person}")
+    private String personRoutingKey;
+
+    @Value("${rabbitmq.exchange.default}")
+    private String defaultExchange;
+
+    public void sendMessage(Object message) {
+        rabbitTemplate.convertAndSend(defaultExchange, personRoutingKey, message);
     }
 
 }
